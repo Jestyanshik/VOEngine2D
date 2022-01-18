e6c94800e0e79ed0d7f83db1ba8a573c965e88d2
jayko <ldzkeymylp@gmail.com>
2022-01-18 11:33:13 +0300

    merged window branch

---
+++ VOEngine/src/core/Window.cpp
@@ -0,0 +1,64 @@
+#include "vopch.h"
+#include "Window.h"
+
+void VOEngine::Window::detectMonitor() {
+	//TODO
+}
+
+void VOEngine::Window::createWindow() {
+	glfwWindowHint(GLFW_RESIZABLE, m_Resizable);
+	glfwWindowHint(GLFW_DECORATED, m_Decorated);
+	glfwWindowHint(GLFW_FOCUSED, m_Focused);
+	glfwWindowHint(GLFW_AUTO_ICONIFY, m_AutoIconify);
+	glfwWindowHint(GLFW_MAXIMIZED, m_Maximized);
+	m_Window = glfwCreateWindow(m_Width, m_Height, m_Title, NULL, NULL);
+}
+
+void VOEngine::Window::setWindowMode(int wm){
+	if (wm == WindowModes::fullscreen) {
+		glfwSetWindowMonitor(m_Window, m_PrimaryMonitor, 0, 0, m_Mode->width, m_Mode->height, m_Mode->refreshRate);
+		//changeSize(m_Mode->width, m_Mode->height);
+	}
+	if (wm == WindowModes::borderless) {
+		glfwSetWindowAttrib(m_Window, GLFW_DECORATED, 0);
+		glfwSetWindowMonitor(m_Window, NULL, 0, 0, m_Mode->width, m_Mode->height, m_Mode->refreshRate);
+	}
+	if (wm == WindowModes::windowed) {
+		glfwSetWindowAttrib(m_Window, GLFW_DECORATED, 1);
+		glfwSetWindowMonitor(m_Window, NULL, (m_Mode->width - m_Width)/2 ,(m_Mode->height - m_Height)/2, m_Width, m_Height, NULL);
+
+	}
+}
+
+bool VOEngine::Window::isKeyPressed(VOEngine::KeyCode key) {
+	int state = glfwGetKey(m_Window, key);
+	if (state == GLFW_PRESS)
+	{
+		return true;
+	}
+	else {
+		return false;
+	}
+}
+
+void VOEngine::Window::setIcon(const char* path) {
+	//TODO
+}
+
+void VOEngine::Window::requestAttention() {
+	glfwRequestWindowAttention(m_Window);
+}
+
+void VOEngine::Window::changeSize(int width, int height) {
+	m_Width = width;
+	m_Height = height;
+	glfwSetWindowSize(m_Window, width, height);
+}
+
+void VOEngine::Window::changeTitle(const char* title) {
+	glfwSetWindowTitle(m_Window, title);
+}
+
+void VOEngine::Window::update() {
+	glfwSwapBuffers(m_Window);
+}
