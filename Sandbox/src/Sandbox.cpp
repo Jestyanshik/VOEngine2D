321b0a07033e8c318c3e872b42a06bd7357d834d
jayko <ldzkeymylp@gmail.com>
2023-01-08 01:58:30 +0300

    123

--- Sandbox/src/Sandbox.cpp
+++ Sandbox/src/Sandbox.cpp
@@ -3,54 +3,32 @@
 
 using namespace VOEngine;
 
-int main() {
-	ResourceManager::Init();
-	
-
-	ResourceManager::addWindow([]() {
-		static ImGuiDockNodeFlags dockspace_flags = ImGuiDockNodeFlags_PassthruCentralNode;
-		ImGuiWindowFlags window_flags = ImGuiWindowFlags_MenuBar | ImGuiWindowFlags_NoDocking;
-
-		ImGuiViewport* viewport = ImGui::GetMainViewport();
-		ImGui::SetNextWindowPos(viewport->Pos);
-		ImGui::SetNextWindowSize(viewport->Size);
-		ImGui::SetNextWindowViewport(viewport->ID);
-		ImGui::PushStyleVar(ImGuiStyleVar_WindowRounding, 0.0f);
-		ImGui::PushStyleVar(ImGuiStyleVar_WindowBorderSize, 0.0f);
-		window_flags |= ImGuiWindowFlags_NoTitleBar | ImGuiWindowFlags_NoCollapse | ImGuiWindowFlags_NoResize | ImGuiWindowFlags_NoMove;
-		window_flags |= ImGuiWindowFlags_NoBringToFrontOnFocus | ImGuiWindowFlags_NoNavFocus;
-
-		if (dockspace_flags & ImGuiDockNodeFlags_PassthruCentralNode)
-			window_flags |= ImGuiWindowFlags_NoBackground;
-
-		ImGui::PushStyleVar(ImGuiStyleVar_WindowPadding, ImVec2(0.0f, 0.0f));
-		ImGui::Begin("DockSpace", nullptr, window_flags);
-		ImGui::PopStyleVar();
-		ImGui::PopStyleVar(2);
-
-		// DockSpace
-		ImGuiIO& io = ImGui::GetIO();
-		if (io.ConfigFlags & ImGuiConfigFlags_DockingEnable) {
-			ImGuiID dockspace_id = ImGui::GetID("MyDockSpace");
-			ImGui::DockSpace(dockspace_id, ImVec2(0.0f, 0.0f), dockspace_flags);
+class Application {
+public:
+	Application(){
+		ResourceManager::Init();
+	}
+	~Application() {
+		ResourceManager::Cleanup();
+	}
+	void run() {
+		BeforeRun();
+		ResourceManager::Run();
+	}
+	void BeforeRun() {
+		ResourceManager::addWindow([]() {
+		ImGui::Begin("Main Window");
+		if (ImGui::Button("Cool button")) {
+			ImGui::Text("hahas");
 		}
 		ImGui::End();
+		});
+	}
+};
 
-		ImGui::Begin("Left");
-		ImGui::Text("Hello, left!");
-		ImGui::End();
-
-		ImGui::Begin("Down");
-		ImGui::Text("Hello, down!");
-		ImGui::End();
-	});
-
-	ResourceManager::getRenderer()->fill(color(1, 0, 0, 1));
-
-	ResourceManager::Run();
-	ResourceManager::Cleanup();
-	
-
+int main() {
+	Application app;
+	app.run();
 	return 0;
 }
 
--- VOEngine/src/core/ResourceManager.cpp
+++ VOEngine/src/core/ResourceManager.cpp
@@ -79,6 +79,33 @@ namespace VOEngine {
 			for (std::function<void(void)> fn : m_RenderQueue) {
 				fn();
 			}
+			static ImGuiDockNodeFlags dockspace_flags = ImGuiDockNodeFlags_PassthruCentralNode;
+			ImGuiWindowFlags window_flags = ImGuiWindowFlags_MenuBar | ImGuiWindowFlags_NoDocking;
+
+			ImGuiViewport* viewport = ImGui::GetMainViewport();
+			ImGui::SetNextWindowPos(viewport->Pos);
+			ImGui::SetNextWindowSize(viewport->Size);
+			ImGui::SetNextWindowViewport(viewport->ID);
+			ImGui::PushStyleVar(ImGuiStyleVar_WindowRounding, 0.0f);
+			ImGui::PushStyleVar(ImGuiStyleVar_WindowBorderSize, 0.0f);
+			window_flags |= ImGuiWindowFlags_NoTitleBar | ImGuiWindowFlags_NoCollapse | ImGuiWindowFlags_NoResize | ImGuiWindowFlags_NoMove;
+			window_flags |= ImGuiWindowFlags_NoBringToFrontOnFocus | ImGuiWindowFlags_NoNavFocus;
+
+			if (dockspace_flags & ImGuiDockNodeFlags_PassthruCentralNode)
+				window_flags |= ImGuiWindowFlags_NoBackground;
+
+			ImGui::PushStyleVar(ImGuiStyleVar_WindowPadding, ImVec2(0.0f, 0.0f));
+			ImGui::Begin("DockSpace", nullptr, window_flags);
+			ImGui::PopStyleVar();
+			ImGui::PopStyleVar(2);
+
+			// DockSpace
+			ImGuiIO& io = ImGui::GetIO();
+			if (io.ConfigFlags & ImGuiConfigFlags_DockingEnable) {
+				ImGuiID dockspace_id = ImGui::GetID("MyDockSpace");
+				ImGui::DockSpace(dockspace_id, ImVec2(0.0f, 0.0f), dockspace_flags);
+			}
+			ImGui::End();
 			ImGui::Render();
 			//temp
 
